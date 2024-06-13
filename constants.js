731dd7667738cec3693401bee79c284742816d3e
R Sarveshvarn <151063233+gdev-kingx@users.noreply.github.com>
2024-06-13 20:23:41 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,80 @@
+const songs = [
+    {
+        name: "Paaraa",
+        artist: "Anirudh Ravichander",
+        src: "/assets/songs/Paaraa.mp3",
+        cover: "/assets/songCovers/Indian.png",
+    },
+    {
+        name: "Come Back Indian",
+        artist: "Anirudh Ravichander",
+        src: "/assets/songs/Come Back Indian.mp3",
+        cover: "/assets/songCovers/Indian.png",
+    },
+    {
+        name: "Kadharalz",
+        artist: "Anirudh Ravichander",
+        src: "/assets/songs/Kadharalz.mp3",
+        cover: "/assets/songCovers/Indian.png",
+    },
+    {
+        name: "Pushpa Pushpa",
+        artist: "Devi Sree Prasad",
+        src: "/assets/songs/Pushpa Pushpa.mp3",
+        cover: "/assets/songCovers/PushpaPushpa.png",
+    },
+    {
+        name: "Lo Lo Local",
+        artist: "Amresh Ganesh",
+        src: "/assets/songs/Lo Lo Lo Local.mp3",
+        cover: "/assets/songCovers/LoLoLoLocal.png",
+    },
+    {
+        name: "Ivan Kaakhi Sattai",
+        artist: "Amresh Ganesh",
+        src: "/assets/songs/Ivan Kaakhi Sattai.mp3",
+        cover: "/assets/songCovers/LoLoLoLocal.png",
+    },
+    {
+        name: "Achacho",
+        artist: "Hiphop Tamizha",
+        src: "/assets/songs/Achacho.mp3",
+        cover: "/assets/songCovers/Achacho.png",
+    },
+    {
+        name: "Naina",
+        artist: "Diljit Dosanjh",
+        src: "/assets/songs/Naina.mp3",
+        cover: "/assets/songCovers/Naina.png",
+    },
+    {
+        name: "Whistle Podu",
+        artist: "Anirudh Ravichander",
+        src: "/assets/songs/Whistle Podu.mp3",
+        cover: "/assets/songCovers/WhistlePodu.png",
+    },
+    {
+        name: "Nee Chuttu Chuttu",
+        artist: "Thaman S",
+        src: "/assets/songs/Nee Chuttu Chuttu.mp3",
+        cover: "/assets/songCovers/NeeChuttuChuttu.png",
+    },
+    {
+        name: "Coolie Disco",
+        artist: "Anirudh Ravichander",
+        src: "/assets/songs/Coolie Disco.mp3",
+        cover: "/assets/songCovers/Disco.png",
+    },
+    {
+        name: "Nakkal Pudichavan",
+        artist: "Hiphop Tamizha",
+        src: "/assets/songs/Nakkal Pudichavan.mp3",
+        cover: "/assets/songCovers/NakkalPudichavan.png",
+    },
+    {
+        name: "Hawa Hawa",
+        artist: "Nivas K Prasanna",
+        src: "/assets/songs/Hawa Hawa.mp3",
+        cover: "/assets/songCovers/HawaHawa.png",
+    },
+]
